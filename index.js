f59344ad19b6add60d38e45eb0d454b1f50ccb25
Max <maxkvt@gmail.com>
2021-01-11 11:26:06 +0300

    Implement minRemoveToMakeValid

--- index.js
+++ index.js
@@ -5,9 +5,58 @@ const grokiing = require('./grokking-algorithms');
 const cronisBits = require('./cronis/bit');
 const bitUtils = require('./cronis/utils');
 
-bitUtils.printHelpTable(11);
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
 
-console.log(cronisBits.generateUniq([10000, 0, 1, 2, 3, 3, 33, 7, 500, 8, 35, 35, 35, 9, 5, 4, 6, 35, 47]));
+const minRemoveToMakeValid = function(s) {
+    let result = s;
+    const open = '(';
+    const close = ')';
+
+    const stack = [];
+    const remove = [];
+
+    for(let i = 0; i < s.length; i++) {
+        const char = s.charAt(i);
+        if (char === open) {
+            stack.push([i, '(']);
+        }
+
+        if (char === close) {
+            if (stack.length == 0) {
+                remove[i] = 1;
+            }
+
+            stack.pop();
+        }
+    }
+
+    if (stack.length > 0) {
+        for(let i = 0; i < stack.length; i++) {
+            let startIndex = stack[i][0];
+            result = setCharAt(result, startIndex, ' ');
+        }
+    }
+
+    if (remove.length > 0) {
+        for(let i = 0; i < remove.length; i++) {
+            if (remove[i] === 1) {
+                result = setCharAt(result, i, ' ');
+            }
+        }
+    }
+
+    return result.replace(/\s/g, '').trim();
+};
+
+console.log(
+minRemoveToMakeValid(')())()((')
+);
+
+//bitUtils.printHelpTable(11);
+//console.log(cronisBits.generateUniq([10000, 0, 1, 2, 3, 3, 33, 7, 500, 8, 35, 35, 35, 9, 5, 4, 6, 35, 47]));
 
 //console.log(dec2bin(switchBits(11)));
 
