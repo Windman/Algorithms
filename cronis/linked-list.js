65470e9ae36bd20e96e1219a9291ee933f52357a
Max <maxkvt@gmail.com>
2021-02-03 21:42:29 +0300

    Pairs

---
+++ cronis/linked-list.js
@@ -0,0 +1,99 @@
+const list = {
+    head: undefined,
+    tail: undefined,
+    
+    addNode: function(data) {
+      const newNode = {
+        next: undefined,
+        prev: undefined,
+        val: data
+      }
+  
+      if (typeof this.head == "undefined") {
+        this.tail = newNode;
+        this.head = newNode;
+        this.head.prev = undefined;
+      } 
+  
+      newNode.prev = this.head;
+      this.head.next = newNode;
+      this.head = newNode;
+      this.tail.prev = undefined;
+    },
+  
+    toArrayFromTail(fromItem) {
+      const listArray = [];
+      let listItem = fromItem; 
+      while(typeof listItem !== "undefined") {
+        listArray.push(listItem.data);
+  
+        listItem = listItem.next;
+      }
+  
+      return listArray;
+    },
+  
+    printFromTailToHead(separator) {
+      console.log(this.toArrayFromTail(this.tail).join(separator));
+    },
+  
+    printFromHeadToTail(separator) {
+     console.log(this.toArrayFromHead(this.head).join(separator));
+    },
+  
+    printData: function() {
+      let listItem = this.tail; 
+      while(typeof listItem !== "undefined") {
+        console.log(listItem.data);
+  
+        listItem = listItem.next;
+      }
+    },
+  
+    middleFromTail(tail) {
+      let slow = tail;
+      let fast = tail;
+  
+      while (typeof fast.next !==  "undefined") {
+        slow = slow.next;
+        fast = fast.next.next;
+  
+        if (typeof fast === "undefined"){
+          return slow.val;
+        }
+      }
+  
+      return slow.val;
+    },
+  
+    middleFromHead(head) {
+      let slow = head;
+      let fast = head;
+  
+      while (typeof fast.next !==  "undefined") {
+        slow = slow.next;
+        fast = fast.next.next;
+  
+        if (typeof fast === "undefined"){
+          return slow.val;
+        }
+      }
+  
+      return slow.val;
+    }
+  }
+  
+  list.addNode(1);
+  list.addNode(2);
+  list.addNode(3);
+  list.addNode(4);
+  list.addNode(5);
+  
+  assert.equal(list.middleFromHead(list.head), 3);
+  
+  list.addNode(6);
+  
+  assert.equal(list.middleFromHead(list.head), 4);
+  
+  list.printFromTailToHead(' -> ');
+  list.printFromHeadToTail(' -> ');
--- index.js
+++ index.js
@@ -7,102 +7,22 @@ const grokiing = require('./grokking-algorithms');
 const cronisBits = require('./cronis/bit');
 const bitUtils = require('./cronis/utils');
 
-const list = {
-  head: undefined,
-  tail: undefined,
-  
-  addNode: function(data) {
-    const newNode = {
-      next: undefined,
-      prev: undefined,
-      val: data
+function pairsBasedOnMap(data, sum) {
+  const map = new Map();
+
+  for (let i = 0; i < data.length; i++) {
+    let diff = sum - data[i];
+    if (diff >= 0) {
+      map.set(data[i], diff);
     }
+  }
 
-    if (typeof this.head == "undefined") {
-      this.tail = newNode;
-      this.head = newNode;
-      this.head.prev = undefined;
-    } 
-
-    newNode.prev = this.head;
-    this.head.next = newNode;
-    this.head = newNode;
-    this.tail.prev = undefined;
-  },
-
-  toArrayFromTail(fromItem) {
-    const listArray = [];
-    let listItem = fromItem; 
-    while(typeof listItem !== "undefined") {
-      listArray.push(listItem.data);
-
-      listItem = listItem.next;
+  for (let i = 0; i < data.length; i++) {
+    if (map.has(data[i])) {
+      console.log(`${map.get(data[i])}, ${data[i]}`);
     }
-
-    return listArray;
-  },
-
-  printFromTailToHead(separator) {
-    console.log(this.toArrayFromTail(this.tail).join(separator));
-  },
-
-  printFromHeadToTail(separator) {
-   console.log(this.toArrayFromHead(this.head).join(separator));
-  },
-
-  printData: function() {
-    let listItem = this.tail; 
-    while(typeof listItem !== "undefined") {
-      console.log(listItem.data);
-
-      listItem = listItem.next;
-    }
-  },
-
-  middleFromTail(tail) {
-    let slow = tail;
-    let fast = tail;
-
-    while (typeof fast.next !==  "undefined") {
-      slow = slow.next;
-      fast = fast.next.next;
-
-      if (typeof fast === "undefined"){
-        return slow.val;
-      }
-    }
-
-    return slow.val;
-  },
-
-  middleFromHead(head) {
-    let slow = head;
-    let fast = head;
-
-    while (typeof fast.next !==  "undefined") {
-      slow = slow.next;
-      fast = fast.next.next;
-
-      if (typeof fast === "undefined"){
-        return slow.val;
-      }
-    }
-
-    return slow.val;
   }
 }
 
-list.addNode(1);
-list.addNode(2);
-list.addNode(3);
-list.addNode(4);
-list.addNode(5);
-
-assert.equal(list.middleFromHead(list.head), 3);
-
-list.addNode(6);
-
-assert.equal(list.middleFromHead(list.head), 4);
-
-list.printFromTailToHead(' -> ');
-list.printFromHeadToTail(' -> ');
+//pairsBasedOnMap([1, 2, 0, 5, 6, -3, 3], 3);
+//pairsBasedOnMap([1, 2, 0, 5, 6, -3, 3], 6);
