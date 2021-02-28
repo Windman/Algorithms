c5ad6e18eedad065f2114ee65b14d671ebc8c64a
Maksim Kilovatiy <mkilovatiy@intermedia.net>
2021-02-28 10:40:03 +0300

    Implement find words with sliding window

--- index.js
+++ index.js
@@ -7,74 +7,27 @@ const grokiing = require('./grokking-algorithms');
 const cronisBits = require('./cronis/bit');
 const bitUtils = require('./cronis/utils');
 
-function bestMin(arr1, arr2) {
-  let i = 0;
-  let j = 0;
-  let bestMin = [arr1[0], arr2[0]];
-
-  while (i < arr1.length && j < arr2.length) {
-    let currDif = Math.abs(arr1[i] - arr2[j]);
-    let bestMinDif = Math.abs(bestMin[0] - bestMin[1]);
-
-    if (currDif < bestMinDif) {
-      bestMin[0] = arr1[i];
-      bestMin[1] = arr2[j];
-    }
-
-    if (arr1[i] > arr2[j]) {
-      j++;
-    } else {
-      i++;
-    }
-  }
-
-  return bestMin;
-}
-
-function findBestSumInterval(arr) {
-  const calculateSum = (start, stop) => {
-    let max = 0;
-
-    for (let i = start; i <= stop; i++) {
-      max = max + arr[i];
-    }
-
-    return max;
-  } 
-  
-  console.log(arr);
-  let next = 0;
+function findWords(words, hashMap) {
+  let result = "";
   let start = 0;
-  let stop = 0;
-  let currentSumm = 0;
-  let prevSumm = 0;
-  let bestSumm = [0, 0, 0];
-  
-  while(next < arr.length) {
-    currentSumm =  prevSumm + arr[next];
+  let end = 0;
+  while (end < words.length) {
 
-    if (currentSumm < prevSumm) {
-      start = next - 1;
-      stop = next;
-    } else {
-      stop++;
-    }
-
-    if (bestSumm[2] < currentSumm) {
-      bestSumm = [start, stop, calculateSum(start, stop)];
-    }
+    Object.keys(hashMap).forEach(word => {
+      if (words.charAt(end) === word.charAt(0)) {
+        let candidate = words.substring(end, end + word.length);
+        if (hashMap[candidate]) {
+          result = result + words.substring(start, end) + ' ' + candidate + ' ';
+          start = end + word.length;
+        } 
+      }
+    });
     
-    next ++;
-
-    console.log(`${start} ${stop} ${bestSumm[2]}`);
-    prevSumm = currentSumm;
+    end++;
   }
 
-  return bestSumm;
+  return result;
 }
 
-console.log(findBestSumInterval([2, -1, 2, -8, 3, -2, 4, -2, 2, 1, -5]));
 
-// console.log(
-//   bestMin([6, 10, 19], [1, 2, 9, 15, 25])
-// );
+console.log(findWords("ILOVECATSANDDOGSSSSS".toLowerCase(), { cats: true, dog: true, i: true }));
